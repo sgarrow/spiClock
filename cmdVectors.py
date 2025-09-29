c1b98ebb385a91800e8c1d5ad86eee525cc3bb86
sgarrow <stangarrow@yahoo.com>
2025-09-28 18:57:46 -0700

    'v1.4.27'. Update docs. Fix bug in shellCmds.

--- cmdVectors.py
+++ cmdVectors.py
@@ -42,7 +42,7 @@ def disconnect():  # Handled directly in the handleClient func so it
                    # is done a func needs to exist. Func never called/runs.
 #############################################################################
 def getVer():
-    VER = ' v1.4.26 - 25-Sep-2025'
+    VER = ' v1.4.27 - 28-Sep-2025'
     return [VER]
 #############################################################################
 
