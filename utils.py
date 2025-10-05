674af1822ada25039453ad1f143a134880d9f70d
sgarrow <stangarrow@yahoo.com>
2025-10-04 18:05:49 -0700

    'v1.5.2'. Commonize spiClock and sprinkler2.

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 import sys
-import time
-import pprint          as pp
 import datetime        as dt
 import multiprocessing as mp
 import threading       as th
+
+import time
 import startStopClock  as cr
 import makeScreen      as ms
 import spiRoutines     as sr
+
 openSocketsLst = []    # Needed for processing close and ks commands.
 #############################################################################
 
@@ -48,45 +49,6 @@ def getActThrds():
         if v is not None:
             rspStr += '   {}\n'.format(k)
     return [rspStr]
-#############################################################################
-
-def displayPics(prmLst):
-    startTimeLst,qs,styleDic,styleLk = prmLst[0], prmLst[1], prmLst[2], prmLst[3]
-    rspStr = getActThrds()
-    stoppedClock = False
-    if 'clockCntrProc' in rspStr[0] or 'lcdUpdateProc' in rspStr[0]:
-        #print('stopping clock')
-        cr.stopClk(qs)
-        stoppedClock = True
-
-    ##################################
-    rspStr = getActThrds()
-    if 'MainThread' not in rspStr[0]:
-        #print('reseting LCD')
-
-        dspIdLst = [ 'hrMSD','hrLSD','mnMSD','mnLSD','scMSD','scLSD' ]
-        sr.hwReset()              # HW Reset
-        for displayID in dspIdLst:
-            sr.swReset(displayID) # SW Reset and the display initialization.
-
-    sr.setBkLight([1])            # Turn on backlight.
-
-    picLst   = [ 'pics/240x320a.jpg', 'pics/240x320b.jpg', 'pics/240x320c.jpg',
-                 'pics/240x320d.jpg', 'pics/240x320e.jpg', 'pics/240x320f.jpg' ]
-
-    dspIdLst = [ 'hrMSD','hrLSD','mnMSD','mnLSD','scMSD','scLSD' ]
-
-    for displayID, pic in zip( dspIdLst, picLst ):
-        data = ms.makePilJpgPicImage(pic)
-        sr.setEntireDisplay( displayID, data, sr.sendDat2ToSt7789 )
-    time.sleep(3)
-    ##################################
-
-    if stoppedClock:
-        #print('starting clock')
-        cr.startClk([startTimeLst,qs,styleDic,styleLk])
-
-    return ['done']
 #############################################################################
 
 def readFileWrk(parmLst, inFile):
@@ -164,14 +126,57 @@ def readFile(parmLst):
     sys.stdout.flush()
     rspStr = readFileWrk(linesToRead, fName)
     return [rspStr]
+#############################################################################
 
 def clearFile(parmLst):
     fName = parmLst[0]
     sys.stdout.flush()
     rspStr = clearFileWrk(fName)
     return [rspStr]
+#############################################################################
 
 def writeFile(fName, inStr):
     with open(fName, 'a', encoding='utf-8') as f:
         f.write( inStr )
         f.flush()
+##########################################t###################################
+
+def displayPics(prmLst):
+    startTimeLst,qs,styleDic,styleLk = prmLst[0], prmLst[1], prmLst[2], prmLst[3]
+    rspStr = getActThrds()
+    stoppedClock = False
+    if 'clockCntrProc' in rspStr[0] or 'lcdUpdateProc' in rspStr[0]:
+        #print('stopping clock')
+        cr.stopClk(qs)
+        stoppedClock = True
+
+    ##################################
+    rspStr = getActThrds()
+    if 'MainThread' not in rspStr[0]:
+        #print('reseting LCD')
+
+        dspIdLst = [ 'hrMSD','hrLSD','mnMSD','mnLSD','scMSD','scLSD' ]
+        sr.hwReset()              # HW Reset
+        for displayID in dspIdLst:
+            sr.swReset(displayID) # SW Reset and the display initialization.
+
+    sr.setBkLight([1])            # Turn on backlight.
+
+    picLst   = [ 'pics/240x320a.jpg', 'pics/240x320b.jpg', 'pics/240x320c.jpg',
+                 'pics/240x320d.jpg', 'pics/240x320e.jpg', 'pics/240x320f.jpg' ]
+
+    dspIdLst = [ 'hrMSD','hrLSD','mnMSD','mnLSD','scMSD','scLSD' ]
+
+    for displayID, pic in zip( dspIdLst, picLst ):
+        data = ms.makePilJpgPicImage(pic)
+        sr.setEntireDisplay( displayID, data, sr.sendDat2ToSt7789 )
+    time.sleep(3)
+    ##################################
+
+    if stoppedClock:
+        #print('starting clock')
+        cr.startClk([startTimeLst,qs,styleDic,styleLk])
+
+    return ['done']
+#############################################################################
+
