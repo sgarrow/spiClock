91c92c60af450e99871026333444cec954ca363b
sgarrow <stangarrow@yahoo.com>
2025-11-26 19:30:25 -0800

    v1.6.13. Update swUpdate.py - part 1.

--- swUpdate.py
+++ swUpdate.py
@@ -1,81 +1,35 @@
-import os
-import zipfile
 import platform
+import zipfile
+import os
 import requests
 import cfg
 #############################################################################
 #############################################################################
 
 def getLatestReleaseInfo(repoOwner, repoName):
-    url = 'https://api.github.com/repos/{}/{}/releases/latest'.format(repoOwner,repoName)
-    response = requests.get( url, timeout = 5 )
 
+    url = 'https://api.github.com/repos/{}/{}/releases/latest'.\
+        format(repoOwner,repoName)
+
+    response = requests.get( url, timeout = 5 )
     if response.status_code == 200:
         latestRelease = response.json()
         return latestRelease['tag_name'], latestRelease['html_url']
     return None, None
 #############################################################################
 
-def downloadZip( dnldToPath, zipFileUrl ):
-
-    fName = zipFileUrl.split('/')[-1]
-    fullyQualifiedFname = dnldToPath + fName
-    response = requests.get( zipFileUrl, timeout = 5 )
-
-    if response.status_code == 200:
-        with open(fullyQualifiedFname,'wb') as outFile:
-            outFile.write(response.content)
-        rspStr = ' Successfully downloaded {} to {}\n'.format(fName,dnldToPath)
-        status = 'SUCCESS'
+def parseReleaseInfo(releaseInfo):
+    latestTag  = releaseInfo[0]
+    releaseUrl = releaseInfo[1]
+    if latestTag:
+        zipUrl = releaseUrl.replace('releases/tag','archive/refs/tags')+'.zip'
     else:
-        rspStr = ' Failed to download {} to {}\n'.format(fName,dnldToPath)
-        status = 'FAIL'
-
-    return rspStr, status, fullyQualifiedFname
+        zipUrl = None
+    return latestTag,releaseUrl,zipUrl
 #############################################################################
 
-def unzipFileTo(unzipToPath, fullyQualifiedFname):
-
-    # Create and Unzip to the parent directory name contained in the zip file itself.
-    #with zipfile.ZipFile(fullyQualifiedFname, 'r') as f:
-    #    f.extractall( unzipToPath )
-
-    # Unzip directly into the specified directory.
-    with zipfile.ZipFile(fullyQualifiedFname, 'r') as f:
-        # Get the name of the top-level folder in the zip
-        topLevelFolder = f.namelist()[0].split('/')[0]
-
-        for member in f.infolist():
-            # Remove the top-level folder from the path
-            memberPath = member.filename
-            if memberPath.startswith(topLevelFolder + '/'):
-                relativePath = memberPath[len(topLevelFolder)+1:]
-            else:
-                relativePath = memberPath
-
-            if relativePath:  # skip the top-level folder itself
-                targetPath = os.path.join(unzipToPath, relativePath)
-                if member.is_dir():
-                    os.makedirs(targetPath, exist_ok=True)
-                else:
-                    os.makedirs(os.path.dirname(targetPath), exist_ok=True)
-                    with open(targetPath, 'wb') as outfile, f.open(member) as source:
-                        outfile.write(source.read())
-
-    rspStr = ' Successfully extracted {} into {}'.\
-             format(fullyQualifiedFname,unzipToPath)
-
-    return rspStr
-#############################################################################
-
-def updateSw():
-
-    rspStr    = ''
-    repoOwner = 'sgarrow'
-    repoName  = 'spiClock'
-    osName    = platform.system()
-
-    ###############
+def getPaths():
+    osName     = platform.system()
     if osName == 'Windows':
         dwnldToPath  = 'C:/01-home/temp/' # pylint: disable=C0103
         unzipToPath  = 'C:/01-home/temp/' # pylint: disable=C0103
@@ -83,33 +37,134 @@ def updateSw():
         dwnldToPath  = os.getcwd() + '/'
         unzipToPath  = os.getcwd() + '/'
     else:
-        rspStr = 'Could not determine OS.'
-        return [rspStr]
-    ###############
+        dwnldToPath  = None
+        unzipToPath  = None
+    return dwnldToPath, unzipToPath
+#############################################################################
 
-    latestTag, releaseUrl = getLatestReleaseInfo( repoOwner, repoName )
-    if latestTag:
-        zipUrl= releaseUrl.replace('releases/tag','archive/refs/tags')+'.zip'
+def downloadZip( dnldToPath, zipFileUrl ):
+    fName = zipFileUrl.split('/')[-1]
+    fullyQualifiedFname = dnldToPath + fName
+    response = requests.get( zipFileUrl, timeout = 5 )
+    status = 'FAIL'
+    if response.status_code == 200:
+        with open(fullyQualifiedFname,'wb') as outFile:
+            outFile.write(response.content)
+        status = 'SUCCESS'
+    return status, fullyQualifiedFname
+#############################################################################
 
-        rspStr += ' Latest release version:      {}\n'.format( latestTag  )
-        rspStr += ' Latest release location:     {}\n'.format( releaseUrl )
-        rspStr += ' Latest release zip location: {}\n'.format( zipUrl     )
+def unzipFileTo(unzipToPath, fullyQualifiedFname):
+    rspStr = ''
+    # Unzip directly into the specified directory.
+    with zipfile.ZipFile(fullyQualifiedFname, 'r') as f:
+        # Get the name of the top-level folder in the zip
+        topLevelFolder = f.namelist()[0].split('/')[0]
+        for member in f.infolist():
+            # Remove the top-level folder from the path
+            memberPath = member.filename
+            if memberPath.startswith(topLevelFolder + '/'):
+                relativePath = memberPath[len(topLevelFolder)+1:]
+            else:
+                relativePath = memberPath
+            if relativePath:  # skip the top-level folder itself
+                targetPath = os.path.join(unzipToPath, relativePath)
+                if member.is_dir():
+                    os.makedirs(targetPath, exist_ok=True)
+                else:
+                    os.makedirs(os.path.dirname(targetPath), exist_ok=True)
+                    if targetPath.split('/')[-1] == 'cfg.cfg':
+                        rspStr += '     Skipping {}\n'.format(targetPath)
+                    else:
+                        with open(targetPath, 'wb') as outfile, f.open(member) as source:
+                            rspStr += '     Writing  {}\n'.format(targetPath)
+                            outfile.write(source.read())
+    return rspStr
+#############################################################################
 
-        fRspStr, sts, fullQualifiedFname = downloadZip(dwnldToPath,zipUrl)
-        rspStr += fRspStr
+def compareVerNums( swVerLst, repoVerLst ):
+    # 0: equal. 1: SW Bigger. 2: Repo Bigger
 
-        if sts == 'SUCCESS':
-            rspStr += unzipFileTo( unzipToPath, fullQualifiedFname )
+    if len(swVerLst) != 3 or len(repoVerLst) != 3: return 3
 
-    else:
-        rspStr = ' Failed to fetch the latest release information from {}.'.\
-                 format( 'github.com/' + repoOwner + '/' + repoName )
+    if swVerLst == repoVerLst:      return 0
 
-    return [rspStr]
+    if swVerLst[0] > repoVerLst[0]: return 1
+    if swVerLst[0] < repoVerLst[0]: return 2
+
+    # swVerLst[0] == repoVerLst[0]: 
+    if swVerLst[1] > repoVerLst[1]: return 1
+    if swVerLst[1] < repoVerLst[1]: return 2
+
+    # swVerLst[1] == repoVerLst[1]: 
+    if swVerLst[2] > repoVerLst[2]: return 1
+    if swVerLst[2] < repoVerLst[2]: return 2
+
+    return 3 # Should never get here,
 #############################################################################
 
 if __name__ == '__main__':
-    ccDict = cfg.getCfgDict('clk')
-    print(ccDict)
-    mnRspStr = updateSw()
-    print(mnRspStr[0])
+
+    def getVer():
+        VER = ' v1.6.12 - 24-Nov-2025'
+        return [VER]
+
+    #verND        = cv.getVer()
+    verND         = getVer()
+    verNDSplit    = verND[0].split('-')
+    swVerAsLst    = [ x.strip() for x in verNDSplit[0].split('.') ]
+    swVerAsLst[0] = swVerAsLst[0][1:]
+    swVerAsIntLst = [int(x) for x in swVerAsLst]
+    print('\n          SW  VER as list = {}'.format( swVerAsIntLst ))
+
+    dwnldToPath,unzipToPath = getPaths()
+    #print(' dwnldToPath, unzipToPath = {}, {}\n'.\
+    #    format( dwnldToPath,unzipToPath ))
+
+    mnRepoOwner = 'sgarrow'
+    mnRepoNames = ['spiClock', 'sharedClientServerCode']
+    mnRepoNames = ['spiClock']
+
+    for ii, mnRepoName in enumerate(mnRepoNames):
+
+        releaseInfo = getLatestReleaseInfo(mnRepoOwner,mnRepoName)
+        #print(' Release Info {}  = {}'.format(ii,releaseInfo))
+
+        tag,url,zipUrl = parseReleaseInfo(releaseInfo)
+
+        if tag == None or url == None:
+            print( '   Failed to get the latest release information from {}.\n'.\
+                 format( 'github.com/' + mnRepoOwner + '/' + mnRepoName ))
+            continue
+
+        #print('   Parsed Release Info:')
+        #print('     tag    = {}'.format( tag    ))
+        #print('     url    = {}'.format( url    ))
+        #print('     zipUrl = {}'.format( zipUrl ))
+        #print()
+        repoVerAsLst    = [ x.strip() for x in tag.split('.') ]
+        repoVerAsLst[0] = repoVerAsLst[0][1:]
+        repoVerAsIntLst = [int(x) for x in repoVerAsLst]
+        print('\n        REPO  VER as list = {}'.format( repoVerAsIntLst ))
+
+        # 0: equal. 1: SW Bigger. 2: Repo Bigger 
+        rsp = compareVerNums( swVerAsLst, repoVerAsLst )
+        myStrLst = [ 'same  as', 'older than', 'newer than', '??? than' ]
+        print('Repo ver is {} running ver. '.format(myStrLst[rsp]))
+        continue
+
+        sts, fullQualifiedFname = downloadZip(dwnldToPath,zipUrl)
+        if sts == 'FAIL':
+            print('   Failed to download {} to {}\n'.\
+                format(zipUrl.split('/')[-1],dwnldToPath))
+            continue
+        print('   Successfully downloaded {} to {}\n'.\
+            format(zipUrl.split('/')[-1],dwnldToPath))
+
+        if sts == 'SUCCESS':
+            rspStr = unzipFileTo( unzipToPath, fullQualifiedFname )
+            print(rspStr)
+            print(' Successfully extracted {} into {}'.
+                     format(fullQualifiedFname,unzipToPath))
+
+    print()
