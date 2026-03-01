7ce9b2c03916e6689cf5454f91efcb3ae0c3b927
sgarrow <stangarrow@yahoo.com>
2026-02-28 19:14:49 -0800

    "Test 1 of release script."

--- release.py
+++ release.py
@@ -1,9 +1,15 @@
 from pathlib import Path
 import subprocess as sp
 import datetime   as dt
-import pprint     as pp
+import pprint     as pp # pylint: disable=W0611
 import sys
 import re
+#############################################################################
+
+def mapTo2D(  flatList, numCols ):
+    # mapTo2D( [1,2,3,4,5,6,7,8], 2 ) = [[1,2],[3,4],[5,6],[7,8]]
+    twoD = [flatList[ii:ii+numCols] for ii in range(0,len(flatList),numCols)]
+    return twoD
 #############################################################################
 
 def printPathInfo(spiClockD,sprinkler2D,sharedD):
@@ -20,16 +26,6 @@ def printPathInfo(spiClockD,sprinkler2D,sharedD):
 #############################################################################
 
 def moveOrCopyFiles( src, dst, fLst, moveOrCopy ):
-
-    #print( '\n Moving shared files.' )
-    #                src,         dst,       fLst,        cmd
-    #moveOrCopyFiles( spiClockDir, sharedDir, expectedUntrackedFs, 'Mov' )
-
-    #print( '\n Copying shared files.' )
-    #dsts = [ spiClockDir, sprinkler2Dir ]
-    #for dstn in dsts:
-    #    #                src,       dst,  fLst,                 cmd
-    #    #moveOrCopyFiles( sharedDir, dstn, expectedUntrackedFs, 'Copy' )
 
     if moveOrCopy not in ['Mov','Copy']:
         print(' moveOrCopy has invalid value.')
@@ -56,6 +52,26 @@ def moveOrCopyFiles( src, dst, fLst, moveOrCopy ):
                 print( '       Copied {}.'.format( file))
 #############################################################################
 
+def tstMoveOrCopyFiles():
+    clkDir   = Path( r'C:\01-home\14-python\temp\tstClkDir' )
+    sprDir   = Path( r'C:\01-home\14-python\temp\tstSprDir' )
+    shrDir   = Path( r'C:\01-home\14-python\temp\tstShrDir' )
+    dsts     = [clkDir, sprDir]
+    fLstStr  = [ 'd1.txt', 'd2.txt', 'd3.txt' ]
+    fLstPath = [ Path(x) for x in fLstStr ]
+
+    print( '\n Moving files.' )
+    #                src     dst     fLst       cmd
+    moveOrCopyFiles( clkDir, shrDir, fLstPath, 'Mov' )
+
+    x = input( 'Press Return to Continue.' )
+
+    print( '\n Copying files.' )
+    for dstn in dsts:
+        #                src      dst   fLst       cmd
+        moveOrCopyFiles( shrDir,  dstn, fLstPath, 'Copy' )
+#############################################################################
+
 def getVerNums( fName, numChanged, numTracked ):
     curVStr   = None
     newVStrwV = None
@@ -67,7 +83,7 @@ def getVerNums( fName, numChanged, numTracked ):
     fileWithVerNum = Path( fName )
 
     try:
-        txt = fileWithVerNum.read_text().split('\n')
+        txt = fileWithVerNum.read_text(encoding='utf-8').split('\n')
     except (FileNotFoundError, IsADirectoryError):
         return curVStr, -2, newVStrwV
 
@@ -104,7 +120,7 @@ def getVerNums( fName, numChanged, numTracked ):
 #############################################################################
 
 def runCommand( cmdLst ):
-    print( '\n Running command: {}\n'.format(' '.join(cmdLst)))
+    #print( '\n Running command: {}\n'.format(' '.join(cmdLst)))
      # If check is true, and the process exits with a non-zero exit code,
      # a CalledProcessError exception will be raised. Attributes of that
      # exception hold the arguments, the exit code, and stdout and stderr
@@ -125,13 +141,10 @@ def runCommand( cmdLst ):
 #############################################################################
 
 def runCommandTst():
-    allTrackedFiles = []
-    cmdLst = [ 'git', 'ls-files' ]
+    #cmdLst = [ 'git', 'ls-files' ]
     cmdLst = [ 'cmd', '/c', 'dir' ]
     err, stdOut, stdErr = runCommand(cmdLst)
-    if not err:
-        stdOutLines = stdOut.split('\n')
-    print( err, stdOut, stdErr )
+    print( 'err = \n', err, '\nstdout = \n', stdOut, '\nstderr = \n',stdErr )
     return err, stdOut, stdErr
     #################################
 def getAllTrackedFs():
@@ -179,42 +192,42 @@ def getUnexpectedUntrackedFs( untracked, expectedUntracked ):
 def getFileLstDict():
     fDict = {
         'trackedFs'            : 
-        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':None, 'len':None,
+        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':[], 'len':None,
         'func':getAllTrackedFs         },
 
         'changedTrackedFs'     :
-        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':None, 'len':None,
+        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':[], 'len':None,
         'func':getChangedTrackedFs     },
 
         'untrackedFs'          :
-        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':None, 'len':None,
+        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':[], 'len':None,
         'func':getUntrackedFs          },
 
         'expectedUntrackedFs'  :
-        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':None, 'len':None,
+        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':[], 'len':None,
         'func':getExpectedUntrackedFs  },
 
         'unexpectedUntrackedFs':
-        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':None, 'len':None,
+        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':[], 'len':None,
         'func':getUnexpectedUntrackedFs},
 
         'trackedPyFs'          :
-        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':None, 'len':None,
+        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':[], 'len':None,
         'func':None                    },
 
         'changedTrackedPyFs'   :
-        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':None, 'len':None,
+        { 'sts':None, 'stdO':None, 'stdE':None, 'fLst':[], 'len':None,
         'func':None                    },
     }
 
-    for k in fDict:
+    for k,v in fDict.items():
         if k in ['unexpectedUntrackedFs', 'trackedPyFs', 'changedTrackedPyFs']:
             continue
-        s,o,e,l = fDict[k]['func']()
-        fDict[k]['sts' ] = s
-        fDict[k]['stdO'] = o
-        fDict[k]['stdE'] = e
-        fDict[k]['fLst'] = l
+        s,o,e,l = v['func']()
+        v['sts' ] = s
+        v['stdO'] = o
+        v['stdE'] = e
+        v['fLst'] = l
 
     k = 'unexpectedUntrackedFs'
     s,o,e,l = fDict[k]['func'](fDict['untrackedFs'       ]['fLst'],
@@ -229,14 +242,79 @@ def getFileLstDict():
     fDict['changedTrackedPyFs']['fLst'] = \
         [x for x in fDict['changedTrackedFs']['fLst'] if x.endswith('.py')]
 
-    for k in fDict:
-        fDict[k]['len'] = len(fDict[k]['fLst'])
+    for k,v in fDict.items():
+        v['len'] = len(v['fLst'])
     return fDict
 #############################################################################
+
+def printFileLstDict(inFileListDict, pEn):
+    #pp.pprint(inFileListDict)
+    printOrder = [
+        'trackedFs',    'trackedPyFs',         'changedTrackedPyFs',
+        'untrackedFs',  'expectedUntrackedFs', 'changedTrackedFs',
+        'unexpectedUntrackedFs'
+    ]
+    groupSize = 2  if pEn else 3
+    width     = 50 if pEn else 25
+
+    for el in printOrder:
+        print( '\n   {} (len  = {}) = '.format(el, inFileListDict[el]['len']))
+        if el == 'trackedFs' and not pEn:
+            print( '   Not printed.  Use /p cmd line arg.')
+            continue
+        #pp.pprint(inFileListDict[el]['fLst'])
+        d2FileList = mapTo2D( inFileListDict[el]['fLst'], groupSize )
+        pStr = ''
+        for group in d2FileList:
+            pStr = [ '{}{}'.format((width-len(x))*' ',x) for x in group ]
+            [print(x,end = '') for x in pStr] # pylint: disable=W0106
+            print()
+#############################################################################
+
+def exitOnErrorsInFileLstDict(inFileListDict):
+    print()
+    doExit = False
+    for k,v in inFileListDict.items():
+        if v['sts']:
+            print( ' {:23} error = {}'.format(k, v['sts']))
+            erMsgLines = v['stdE'].split('\n')
+            for errLine in  erMsgLines:
+                print('     {}'.format(errLine))
+            print()
+            doExit = True
+    if doExit:
+        print( ' Exiting, RE: Error.\n' )
+        sys.exit()
+#############################################################################
+
+def printStdOutOrStdErr( hasError, stdOut, stdErr ):
+
+    if hasError:
+        print( '   error = {}'.format(hasError))
+        msgLines = stdErr.split('\n')
+    else:
+        msgLines = stdOut.split('\n')
+
+    for theLine in  msgLines:
+        print('     {}'.format(theLine))
+    print()
+
+    if hasError:
+        print( ' Exiting, RE: Error.\n' )
+        sys.exit()
+#############################################################################
+
 
 if __name__ == '__main__':
 
     #runCommandTst()
+    #tstMoveOrCopyFiles()
+    #sys.exit()
+
+    arguments  = sys.argv
+    scriptName = arguments[0]
+    userArgs   = arguments[1:]
+    prnEn      = (len(userArgs) > 0 and '/p' in userArgs)
 
     print( '\n Building Paths' )
     spiClockDir   = Path( r'C:\01-home\14-python\gitTrackedCode\spiClock' )
@@ -244,49 +322,36 @@ if __name__ == '__main__':
     sharedDir     = Path( r'C:\01-home\14-python\gitTrackedCode\sharedClientServerCode' )
     #printPathInfo(spiClockDir,sprinkler2Dir,sharedDir)
     #########################################
+
     print( '\n Getting changed and untracked files.' )
     fLstDict = getFileLstDict()
-    pp.pprint(fLstDict)
-
-    printOrder = [
-        'trackedFs',        'trackedPyFs',
-        'changedTrackedFs', 'changedTrackedPyFs',
-        'untrackedFs',      'expectedUntrackedFs', 'unexpectedUntrackedFs'
-    ]
-    for k in printOrder:
-        print( '\n {} (len  = {}) = \n'.format(k, fLstDict[k]['len']))
-        pp.pprint(fLstDict[k]['fLst'])
-
-    print()
-    for k in printOrder:
-        print( ' {:23} error = {}'.format(k, fLstDict[k]['sts']))
-        if fLstDict[k]['sts']:
-            errMsgLines = fLstDict[k]['stdE'].split('\n')
-            for l in  errMsgLines:
-                print('     {}'.format(l))
-            print()
-
+    printFileLstDict( fLstDict, prnEn )
+    exitOnErrorsInFileLstDict( fLstDict )
     #########################################
+
     if fLstDict['unexpectedUntrackedFs']['len'] > 0:
         print( '\n unexpected/untracked files present.')
-        print( ' Continuing will not add them.')
-        print( ' Add from cmd line <git add fName>')
-        goOn = input( ' Continue (y/n)? -> ')
+        print( '   Continuing will not add them.')
+        print( '   Add from cmd line like this <git add fName>')
+        goOn = input( ' Continue (y/n)? -> ' )
         if goOn != 'y':
             sys.exit()
-        print(' Continuing')
 
     if fLstDict['changedTrackedFs']['len'] == 0:
-        print( ' No tracked/changed files present.')
-        print( ' Continuing will bump rev and thus cmdVectors.py will change.')
-        goOn = input( ' Continue (y/n)? -> ')
+        print( ' No tracked/changed files present.' )
+        print( ' Continuing will bump rev and thus cmdVectors.py will change.' )
+        goOn = input( ' Continue (y/n)? -> ' )
         if goOn != 'y':
             sys.exit()
-        print(' Continuing')
     #########################################
-    print( '\n Getting curr/new version number and Date' )
 
+    print( '\n Getting curr/new Version Number and Date' )
     fileWithVerNumInIt = 'cmdVectors.py' # pylint: disable=C0103
+
+    if fileWithVerNumInIt not in fLstDict['changedTrackedFs']['fLst']:
+        print( '   Adding {} to changedTrackedFs'.format(fileWithVerNumInIt))
+        fLstDict['changedTrackedFs']['fLst'].append('cmdVectors.py')
+        fLstDict['changedTrackedFs']['len'] = fLstDict['changedTrackedFs']['len'] + 1
 
     curVerStr, pcntChanged, newVerStr = \
     getVerNums( fileWithVerNumInIt,
@@ -294,42 +359,64 @@ if __name__ == '__main__':
                 fLstDict['trackedPyFs']['len']
     )
     date = dt.datetime.now().strftime( '%d-%b-%Y' )
-    print(' Percent of tracked .py files chanhged = {}%'.format(pcntChanged))
-    print(' Old/New Version Numbers = {}/{}'.format(curVerStr, newVerStr))
+    print('   Percent of tracked .py files chanhged = {}%'.format(pcntChanged))
+    print('   Old/New Version Numbers = {}/{}'.format(curVerStr, newVerStr))
 
     if pcntChanged == -1:
-        print( '\n Error getting ver num (divide by zero).\n')
+        print( '\n   Error getting ver num (divide by zero).\n')
     if pcntChanged == -2:
-        print( '\n Error getting ver num (file not found).\n')
+        print( '\n   Error getting ver num (file not found).\n')
     if pcntChanged == -3:
-        print( '\n Error getting ver num (search string not found).\n')
+        print( '\n   Error getting ver num (search string not found).\n')
     if pcntChanged < 0:
+        print( ' Exiting, RE: Error.\n' )
         sys.exit()
     #########################################
 
-    print( '\n Updating app ver and date in {}'.format(fileWithVerNumInIt) )
+    print( '\n Updating app Version and Date in {}'.format(fileWithVerNumInIt) )
     fileToChangeVerNumIn = Path( fileWithVerNumInIt )
-    text = fileToChangeVerNumIn.read_text()
-    new_text = re.sub( r"VER = .*", f"VER = '{newVerStr} - {date}'", text )
-    fileToChangeVerNumIn.write_text(new_text)
+    text = fileToChangeVerNumIn.read_text(encoding='utf-8')
+    new_text = re.sub( r"VER = .*", f"VER = '{newVerStr} - {date}'", text ) # pylint: disable=W1405
+    fileToChangeVerNumIn.write_text(new_text, encoding='utf-8')
     #########################################
 
-    print( '\n Adding, committing and pushing appropriate files.' )
+    print( '\n Adding appropriate files.' )
     cmdBaseLst = [ 'git', 'add' ]
-    for f in fLstDict['changedTrackedFs']:
+    for f in fLstDict['changedTrackedFs']['fLst']:
         cmd = cmdBaseLst + [f]
-        print(cmd)
-        #stdOut,stdErr = runCommand(cmd)
-        #print( 'stdOut = \n{}'.format(stdOut ))
-        #print( 'stdErr = \n{}'.format(stdErr ))
-    #cmd = [ 'git', 'commit', '*', '--no-verify', '-m', commitMsg  ]
-    commitTxt = 'Testing new release script.' # pylint: disable=C0103
-    cmd = [ 'git', 'commit', '*', '--no-verify', '-m', commitTxt  ]
-    print(cmd)
-    #@rem only needed on first push
-    #@rem git branch -M main
-    #@rem git remote add origin https://github.com/sgarrow/spiClock.git
+        print('   {}'.format(' '.join(cmd)))
+        hasErr, stdO, stdE = runCommand(cmd)
+        printStdOutOrStdErr( hasErr, stdO, stdE )
+    #########################################
 
-    #git remote set-url origin https://github.com/sgarrow/spiClock.git
-    #git push -u origin main
+    commitTxt = input( '\n Enter commit message -> ' )
+    commitTxtWithQuotes = r'"{}"'.format(commitTxt)
+    #########################################
+
+    print( '\n Committing.' )
+    cmd = [ 'git', 'commit', '--no-verify', '-m', commitTxtWithQuotes  ]
+    print('   {}'.format(' '.join(cmd)))
+    hasErr, stdO, stdE = runCommand(cmd)
+    printStdOutOrStdErr( hasErr, stdO, stdE )
+    #########################################
+
+    #@rem only needed on first push
+    #@rem git xx_branch -M main
+    #@rem git remote add origin https://github.com/sgarrow/spiClock.git
+    #########################################
+
+    print( '\n Setting GitHub URL.' )
+    cmd = ['git', 'remote', 'set-url', 'origin', 'https://github.com/sgarrow/spiClock.git']
+    print('   {}'.format(' '.join(cmd)))
+    hasErr, stdO, stdE = runCommand(cmd)
+    printStdOutOrStdErr( hasErr, stdO, stdE )
+    #########################################
+
+    print( '\n Pushing to GitHub.' )
+    cmd = ['git', 'push', '-u', 'origin', 'main'                                   ]
+    print('   {}'.format(' '.join(cmd)))
+    hasErr, stdO, stdE = runCommand(cmd)
+    printStdOutOrStdErr( hasErr, stdO, stdE )
 #############################################################################
+
+    print( '\n Release Successful. \n' )
