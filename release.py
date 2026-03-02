e2e10a3d2717f149e6e639acb6a5e81fdfa025ef
sgarrow <stangarrow@yahoo.com>
2026-03-01 18:59:10 -0800

    "v1.6.51. Add function: lookForDiffs"

--- release.py
+++ release.py
@@ -1,4 +1,5 @@
-from pathlib import Path
+from itertools import combinations
+from pathlib   import Path
 import subprocess as sp
 import datetime   as dt
 import pprint     as pp # pylint: disable=W0611
@@ -304,12 +305,53 @@ def printStdOutOrStdErr( hasError, stdOut, stdErr ):
         sys.exit()
 #############################################################################
 
+def lookForDiffs( dirsToComp, fLstStr ):
+    print( '\n Looking for diffs among shared files.\n' )
+
+    fLstPath = [ Path(x) for x in fLstStr ]
+    combSet  = combinations(dirsToComp, 2)
+    width    = 37
+    slash    = '\\'
+
+    for comb in combSet:
+
+        combAsStr0    = str(comb[0])
+        combAsStr1    = str(comb[1])
+
+        slashIndeces0 = [idx for idx,ch in enumerate(combAsStr0) if ch == slash]
+        slashIndeces1 = [idx for idx,ch in enumerate(combAsStr1) if ch == slash] 
+
+        comb0Root     = combAsStr0[ slashIndeces0[-1]: ]
+        comb1Root     = combAsStr1[ slashIndeces1[-1]: ]
+
+        for file in fLstPath:
+
+            f0 =  comb[0] / file  # A Path join. All 3 vars type Path.
+            f1 =  comb[1] / file  # A Path join. All 3 vars type Path.
+
+            text0 = f0.read_text( encoding='utf-8' )
+            text1 = f1.read_text( encoding='utf-8' )
+            equal = text0 == text1
+
+            pStr0 = '   {}\\{}{}'.format( comb0Root, file, (width-len(comb0Root)-len(str(file))) * ' ' )
+            pStr1 = '   {}\\{}'.format(   comb1Root, file )
+
+            print(pStr0, end ='')
+            if equal:
+                print( '==',end = '' )
+            else:
+                print( '!=', end = '' )
+            print(pStr1, end ='')
+
+            print()
+        print('  ##########')
+#############################################################################
+
 
 if __name__ == '__main__':
 
     #runCommandTst()
     #tstMoveOrCopyFiles()
-    #sys.exit()
 
     arguments  = sys.argv
     scriptName = arguments[0]
@@ -320,13 +362,16 @@ if __name__ == '__main__':
     spiClockDir   = Path( r'C:\01-home\14-python\gitTrackedCode\spiClock' )
     sprinkler2Dir = Path( r'C:\01-home\14-python\gitTrackedCode\sprinkler2' )
     sharedDir     = Path( r'C:\01-home\14-python\gitTrackedCode\sharedClientServerCode' )
-    #printPathInfo(spiClockDir,sprinkler2Dir,sharedDir)
+    printPathInfo(spiClockDir,sprinkler2Dir,sharedDir)
     #########################################
 
     print( '\n Getting changed and untracked files.' )
     fLstDict = getFileLstDict()
     printFileLstDict( fLstDict, prnEn )
     exitOnErrorsInFileLstDict( fLstDict )
+
+    lookForDiffs( [sharedDir, spiClockDir, sprinkler2Dir], fLstDict['expectedUntrackedFs']['fLst'])
+    #sys.exit()
     #########################################
 
     if fLstDict['unexpectedUntrackedFs']['len'] > 0:
